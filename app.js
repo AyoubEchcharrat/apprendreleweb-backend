c72bc037ca25a63f29d8555b889f8e647954ac0e
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-10-05 17:10:29 +0200

    bug fix on CORS, modifying "if method OPTIONS"

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    if (res.Method == "OPTIONS") {
-      w.WriteHeader(http.StatusOK)
+    if (req.method == "OPTIONS") {
+      res.sendStatus(http.StatusOK)
       return
   }
     next();
