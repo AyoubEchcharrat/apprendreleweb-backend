cadf33b6ed821724669163a5eb249aa5c741a073
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-10-30 12:25:56 +0100

    modify multer generating unique name method

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -11,10 +11,10 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    const uniqueFileName = Date.now() + '.' + extension;
+    callback(null, uniqueFileName);
   }
 });
 
-module.exports = multer({storage: storage}).single('imageurl');
+module.exports = multer({ storage: storage }).single('imageurl');
