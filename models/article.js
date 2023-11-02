f7de63a614f45aef7e1037dcde545d9e2e395c10
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-11-02 12:32:03 +0100

    add imageurl to required

--- models/article.js
+++ models/article.js
@@ -4,7 +4,7 @@ const articlesSchema = mongoose.Schema({
     content : {type: String, required: true},
     title : {type:String, required : true},
     tags : {type:Array, required : true},
-    imageurl : {type:String},
+    imageurl : { type: String, required: true },
     userId: { type: String, required: true },
     date: {type:String,required: true}
 })
