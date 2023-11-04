f12d16965c8cecec07e18a256cd466d111695bc1
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-11-04 17:00:03 +0100

    add homepage and /articles to sitemap

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -9,11 +9,22 @@ router.get('/sitemap.xml', async (req, res) => {
   try {
     const smStream = new SitemapStream({ hostname: 'https://apprendreleweb.ayoub-echcharrat.fr' });
 
+    smStream.write({
+        url: '/',
+        changefreq: 'daily',
+        priority: 1.0, 
+    });
+    smStream.write({
+        url: '/articles',
+        changefreq: 'daily',
+        priority: 0.7, 
+    });
+
     const articles = await Article.find(); 
 
     articles.forEach((article) => {
       smStream.write({
-        url: `/article/${article._id}`, 
+        url: `/articles/${article._id}`, 
         changefreq: 'daily', 
         priority: 0.7, 
         lastmod: article.date, 
